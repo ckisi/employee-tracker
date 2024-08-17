b1c790cac34bc5e5b1cd0db873050e9ab59153a7
Lain <ckisielius@gmail.com>
2024-08-17 17:37:29 -0400

    added add methods and added inquirer prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,6 +6,6 @@ INSERT INTO roles (title, salary, deparment)
 VALUES ('Developer', 100000.00, 2), 
        ('Code Tester', 90000.00, 2);
 
-INSERT INTO employees (first_name, last_name, role_id) 
-VALUES ('John', 'Smith', 1), 
-       ('Jane', 'Doe', 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES ('John', 'Smith', 1, NULL), 
+       ('Jane', 'Doe', 2, 1);
--- index.js
+++ index.js
@@ -17,24 +17,122 @@ const pool = new Pool(
 
 pool.connect();
 
-Department.view(pool);
-Role.view(pool);
-Employee.view(pool);
+class CLI {
+  static async initialPrompt() {
+    const data = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 
+          'View all roles', 
+          'View all employees', 
+          'Add a department', 
+          'Add a role', 
+          'Add an employee', 
+          'Update an employee role'],
+      },
+    ]);
+    return data;
+  }
 
-/*
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'option',
-      message: 'What would you like to do?',
-      choices: ['View all departments', 
-        'View all roles', 
-        'View all employees', 
-        'Add a department', 
-        'Add a role', 
-        'Add an employee', 
-        'Update an employee role'],
-    },
-  ])
-*/
+  static async addDepartmentPrompt() {
+    const data = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of the department?',
+        },
+      ]);
+      return data;
+  }
+
+  static async addRolePrompt() {
+    const data = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of the role?',
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: 'What is the salary of the role?',
+        },
+        {
+          type: 'input',
+          name: 'department',
+          message: 'Which department does the role belong to?',
+        },
+      ]);
+      return data;
+  }
+
+  static async addEmployeePrompt() {
+    const data = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'firstName',
+          message: `What is the employee's first name?`,
+        },
+        {
+          type: 'input',
+          name: 'lastName',
+          message: `What is the employee's last name?`,
+        },
+        {
+          type: 'input',
+          name: 'role',
+          message: `What is the employee's role?`,
+        },
+        {
+          type: 'input',
+          name: 'manager',
+          message: `Who is the employee's manager?`,
+        },
+      ]);
+      return data;
+  }
+
+  static async updateEmployeePrompt() {
+    const data = await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'update',
+          message: 'Choose an employee to update',
+          choices: [],
+        },
+      ]);
+      return data;
+  }
+}
+
+const data = CLI.initialPrompt();
+
+switch(data) {
+  case 'View all departments':
+    Department.view(pool);
+    break;
+  case 'View all roles':
+    Role.view(pool);
+    break;
+  case 'View all employees':
+    Employee.view(pool);
+    break;
+  case 'Add a department':
+    const department = CLI.addDepartmentPrompt();
+    break;
+  case 'Add a role':
+    const role = CLI.addRolePrompt();
+    break;
+  case 'Add an employee':
+    const employee = CLI.addEmployeePrompt();
+    break;
+  case 'Update an employee role':
+    break;
+}
